436d8e2119b095e9d524313e0a49b14063d404dc
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2021-10-17 19:40:06 +0530

    Array Implementation of Stack

--- Stacks/ArrayImplementaion.c
+++ Stacks/ArrayImplementaion.c
@@ -1,10 +1,49 @@
 // Array Implementation of Stack
 
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define size 10
+
+int Stack[size];
+int top = -1;
+int data;
+
+
+int push(int data){
+    if (top == size - 1){
+        printf("Stack Overflow!");
+    }   
+    else{
+        top = top + 1;
+        Stack[top] =  data;
+    }
+
+}
+
+
+int pop(){
+    if (top == -1){
+        printf("Stack Undeflow!");
+    }
+    else{
+        data = Stack[top];
+        top = top - 1;
+    }
+    return data;
+}
+
+
+int peek(){
+    if (top == -1){
+        printf("Stack Underflow!");
+    }
+    else{
+        data = Stack[top];
+        return data;
+    }
+}
 
 
 void main(){
-    int a = 2;
-    printf(a);
+    //ENTER FUNCTIONS HERE TO USE THEM AS YOUR WISH
 }
